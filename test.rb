29647edeb159d17b356c5362c9401c33726133d5
SuscipiamSingularitatem <26754065+SuscipiamSingularitatem@users.noreply.github.com>
2017-12-16 14:37:01 +1100

    Move functionality to its own module/file

--- test.rb
+++ test.rb
@@ -1,25 +1,9 @@
-require "curb"
+#!/usr/bin/env ruby
+
 require "json"
+require "./e6crawl.rb"
 
-settings = JSON[File.read("settings.json")]
+PRETTY_JSON = {space: " ", object_nl: "\n", array_nl: "\n", indent: "\t"}
 
-query = {
-	"limit" => 1,
-	"tags" => "yoshi"
-}
-
-unless settings["username"].nil? || settings["apikey"].nil?
-	query["login"] = settings["username"]
-	query["password_hash"] = settings["apikey"]
-end
-if settings["ignore_tag_cat"].nil? || !settings["ignore_tag_cat"]
-	query["typed_tags"] = true
-end
-
-http = Curl.get("https://e621.net/post/index.json", query) do |http|
-	http.headers["User-Agent"] = "e6l-ruby/0.1 (by @YoshiRulz on e621)"
-end
-output = JSON[http.body_str]
-
-puts output.length
-puts output[0]["tags"]
+output = E621Crawler::Post.index(tags: "yoshi", limit: 1)
+puts JSON.generate output[0]["tags"], PRETTY_JSON
