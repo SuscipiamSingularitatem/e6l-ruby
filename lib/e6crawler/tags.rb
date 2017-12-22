26df4649669658a4b5a046812a303cddb1a5ff90
SuscipiamSingularitatem <26754065+SuscipiamSingularitatem@users.noreply.github.com>
2017-12-22 14:07:41 +1100

    Add @e926_nsfw tag to yard

--- lib/e6crawler/tags.rb
+++ lib/e6crawler/tags.rb
@@ -2,6 +2,7 @@ module E621Crawler
 	# Contains the functionality of e621.net/tags/*.
 	class Tags
 		# Interfaces with {https://e621.net/tag/show.json}.
+		# @e926_nsfw May request NSFW material from e926.net, even with safe_only.
 		# @return Hash the tag's data
 		def Tags.show_id(id)
 			E621Crawler.http_get_json("https://e#{E6lSettings.get.safe_only ? "926" : "621"}.net/tag/show.json",
