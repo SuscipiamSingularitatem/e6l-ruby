a985ff46248df4c547828e6482c97d3dd58f53ea
SuscipiamSingularitatem <26754065+SuscipiamSingularitatem@users.noreply.github.com>
2017-12-22 14:10:26 +1100

    Change test to thumbnails, now including crawler namespace

--- lib/test.rb
+++ lib/test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 ["e6crawler", "qtgui", "settings"].each do |f| require_relative "#{f}.rb" end
+include E621Crawler
 
-output = E621Crawler::Post.index(tags: "#{["mlp", "rainbow_dash_(mlp)", "twilight_sparkle_(mlp)", "mario_bros", "yoshi", "canine", "feline", "equine", "shark"].sample} -animated", metatags: {order: ["favcount", "score"].sample}, limit: 1)
-E6lQtGUI.single_sample(output[0])
+output = Post.index(tags: "#{["mlp", "rainbow_dash_(mlp)", "twilight_sparkle_(mlp)", "mario_bros", "yoshi", "canine", "feline", "equine", "shark"].sample} -animated", metatags: {order: ["favcount", "score"].sample}, limit: 1)
+E6lQtGUI.single_preview(output[0])
